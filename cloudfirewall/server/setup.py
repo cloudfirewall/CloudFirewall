e8da896461b0926beb93e64a5b3059e7b9fce58b
Gagan607 <gaganbikram.gbs@gmail.com>
2021-05-11 18:29:38 +0545

    commit

---
+++ cloudfirewall/server/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup,find_packages
+
+setup(
+     name='Cloudfirewall-Server',  
+     version='0.0.1',
+     author="Cloud FireWall",
+     author_email="info@cloudfirewall.io",
+     description="CloudFirewall-Server packages",
+     long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+     url="https://github.com/cloudfirewall/CloudFirewall",
+     packages=find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: Free To Use But Restricted",
+         "Operating System :: OS Independent",
+     ],
+ )

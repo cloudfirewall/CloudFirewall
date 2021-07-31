aa003719fad4d52088f7cf4990bc8fd551533536
Gagan607 <gaganbikram.gbs@gmail.com>
2021-07-31 08:54:55 +0545

    Create setup.py file

---
+++ cloudfirewall/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup,find_packages
+
+setup(
+     name='CloudFirewall',  
+     version='0.1',
+     author="Cloud FireWall",
+     author_email="info@cloudfirewall.io",
+     description="CloudFirewall-Agent packages",
+     long_description=open('README.txt','r').close() + '\n\n' + open('CHANGELOG.txt','r').close(),
+     url="https://github.com/cloudfirewall/CloudFirewall",
+     packages=find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: Free To Use But Restricted",
+         "Operating System :: OS Independent",
+         
+     ],
+ )

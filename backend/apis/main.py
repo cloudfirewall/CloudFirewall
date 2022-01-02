419863c65261b612176bcc8665fdd91eb027156c
Shrest4647 <074bct544.sunil@pcampus.edu.np>
2022-01-02 16:00:50 +0545

    Enabled CORS middleware in FastAPI Server and changed the default security group sent by the server.

--- backend/apis/main.py
+++ backend/apis/main.py
@@ -2,9 +2,23 @@ from .database import engine
 from .routers import instances, securityGroups, admin
 from fastapi import FastAPI
 from . import models
-
+from starlette.middleware.cors import CORSMiddleware
+from cloudfirewall.common.util import env_to_list
+import os
 
 app = FastAPI()
+
+ALLOWED_ORIGINS = env_to_list(os.environ.get('ALLOWED_ORIGINS', '*'))
+ALLOWED_METHODS = env_to_list(os.environ.get('ALLOWED_METHODS', '*'))
+ALLOWED_HEADERS = env_to_list(os.environ.get('ALLOWED_HEADERS', '*'))
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=ALLOWED_ORIGINS,
+    allow_credentials=True,
+    allow_methods=ALLOWED_METHODS,
+    allow_headers=ALLOWED_HEADERS,
+)
 app.include_router(instances.router)
 app.include_router(securityGroups.router)
 app.include_router(admin.router)
--- cloudfirewall/server/plugins/nftables/servicer.py
+++ cloudfirewall/server/plugins/nftables/servicer.py
@@ -1,7 +1,11 @@
 import logging
 
+from backend.apis import models
+from backend.apis.utils import get_db
 from cloudfirewall.grpc import firewall_pb2_grpc
-from cloudfirewall.grpc.firewall_pb2 import FirewallFetchResponse, Policy, FirewallStatusAck
+from cloudfirewall.grpc.firewall_pb2 import FirewallFetchResponse, Policy, Chain, Protocol, FirewallStatusAck, FirewallRule
+from backend.apis.cruds.securityGroups import readSecurityGroup, readSecurityGroupById
+from backend.apis.database import SessionLocal, engine
 
 
 class FirewallServicer(firewall_pb2_grpc.FirewallServicer):
@@ -13,7 +17,13 @@ class FirewallServicer(firewall_pb2_grpc.FirewallServicer):
 
     def FetchLatestRules(self, request, context):
         self.logger.info("FetchLatestRules request: %s, peer: %s", request.request_id, context.peer())
-        return FirewallFetchResponse(inbound=Policy.DROP, outbound=Policy.ACCEPT)
+        node_id = request.node_id
+        db = SessionLocal()
+        all_security_groups = db.query(models.SecurityGroups).filter(models.SecurityGroups.id == node_id).first()
+        print(f'securityGroups: {all_security_groups}')
+        myrule1 = FirewallRule(chain=Chain.INPUT, protocol=Protocol.TCP, ipRange="0.0.0.0/0", port=23, policy=Policy.DROP)
+        myrule2 = FirewallRule(chain=Chain.OUTPUT, protocol=Protocol.TCP, ipRange="0.0.0.0/0", port=23, policy=Policy.DROP)
+        return FirewallFetchResponse(inbound=Policy.DROP, outbound=Policy.ACCEPT, rules=[myrule1, myrule2])
 
     def SendFirewallStatus(self, request, context):
         self.logger.info("SendFirewallStatus request: %s, peer: %s", request.request_id, context.peer())
